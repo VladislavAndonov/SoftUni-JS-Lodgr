a006f2b0b663036866a4b890578c1edd7167c088
Vladislav Andonov <wladoandonov@gmail.com>
2024-12-19 23:59:40 +0200

    Updated styles

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Space } from '../../types/space';
 import { SearchBarComponent } from '../../core/shared/search-bar/search-bar.component';
@@ -11,25 +11,24 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   featuredSpaces: Space[] = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService
-      .getAllSpaces('Spaces')
-      .then((snapshot) => {
-        this.featuredSpaces = snapshot.docs.map(
-          (doc) =>
-            ({
-              id: doc.id,
-              ...doc.data(),
-            } as Space)
-        );
-      })
-      .catch((error) => {
-        console.error('Error fetching spaces:', error);
-      });
-  }  
+    this.loadFeaturedSpaces();
+  }
+
+  async loadFeaturedSpaces() {
+    try {
+      const snapshot = await this.apiService.getAllSpaces('Spaces');
+      this.featuredSpaces = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Space[];
+    } catch (error) {
+      console.error('Error fetching spaces:', error);
+    }
+  }
 }
